b1f82a6dd191109398dac2af9ce9719fab1f1cb0
maty21 <maty21@gmail.com>
2019-07-03 14:21:08 +0300

    add tests from pipeline-driver and add travis

---
+++ index.js
@@ -0,0 +1,9 @@
+const NodesMap = require('./lib/nodes/nodes-map');
+const NodeStates = require('./lib/const/NodeStates');
+const NodeTypes = require('./lib/nodes/index');
+
+module.exports = {
+    NodesMap,
+    NodeStates,
+    NodeTypes
+};
