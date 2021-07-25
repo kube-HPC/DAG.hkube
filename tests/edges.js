3f7709ba9c8338b6c091eb4d0fe4b1ef199dc472
NassiHarel <nassi9@gmail.com>
2021-07-25 09:24:05 +0300

    feat: restructure

--- tests/edges.js
+++ tests/edges.js
@@ -1,75 +1,73 @@
 const { expect } = require('chai');
-const NodesMap = require('../lib/graph/nodes-map');
+const NodesMap = require('../lib/graph/dag');
 const pipelines = require('./pipelines.json');
 
-describe('DAG', () => {
-    describe('Edges', () => {
-        it('should create edge', () => {
-            const pipeline = pipelines.find(p => p.name === 'simple-flow');
-            const n1 = pipeline.nodes[0].nodeName;
-            const n2 = pipeline.nodes[3].nodeName;
-            const dag1 = new NodesMap(pipeline);
-            const edge1 = dag1.getEdge(n1, n2);
-            const dag2 = new NodesMap({ ...pipeline, edges: [{ source: n1, target: n2 }] });
-            const edge2 = dag2.getEdge(n1, n2);
-            expect(edge1).to.be.undefined;
-            expect(edge2).to.have.property('types');
-        });
-        it('should getEdge', () => {
-            const pipeline = pipelines.find(p => p.name === 'simple-flow');
-            const n1 = pipeline.nodes[0].nodeName;
-            const n2 = pipeline.nodes[1].nodeName;
-            const nodesMap = new NodesMap(pipeline);
-            const edge = nodesMap.getEdge(n1, n2);
-            expect(edge).to.have.property('types');
-        });
-        it('should setEdge', () => {
-            const pipeline = pipelines.find(p => p.name === 'simple-flow');
-            const n1 = pipeline.nodes[0].nodeName;
-            const n2 = pipeline.nodes[1].nodeName;
-            const nodesMap = new NodesMap(pipeline);
-            nodesMap.setEdge(n1, n2, { prop: 5 });
-            const edge = nodesMap.getEdge(n1, n2)
-            expect(edge).to.have.property('prop');
-            expect(edge).to.not.have.property('types');
-        });
-        it('should updateEdge', () => {
-            const pipeline = pipelines.find(p => p.name === 'simple-flow');
-            const n1 = pipeline.nodes[0].nodeName;
-            const n2 = pipeline.nodes[1].nodeName;
-            const nodesMap = new NodesMap(pipeline);
-            nodesMap.updateEdge(n1, n2, { prop: 5 });
-            const edge = nodesMap.getEdge(n1, n2)
-            expect(edge).to.have.property('prop');
-            expect(edge).to.have.property('types');
-        });
-        it('should get edge types', () => {
-            const pipeline = pipelines.find(p => p.name === 'simple-flow');
-            const n1 = pipeline.nodes[0].nodeName;
-            const n2 = pipeline.nodes[1].nodeName;
-            const nodesMap = new NodesMap(pipeline);
-            const types = nodesMap.getEdgeTypes(n1, n2);
-            expect(types).to.have.lengthOf(2);
-        });
-        it('should get empty edge types', () => {
-            const pipeline = pipelines.find(p => p.name === 'simple-flow');
-            const n1 = pipeline.nodes[0].nodeName;
-            const n2 = pipeline.nodes[3].nodeName;
-            const nodesMap = new NodesMap(pipeline);
-            const types = nodesMap.getEdgeTypes(n1, n2);
-            expect(types).to.have.lengthOf(0);
-        });
-        it('should getEdges', () => {
-            const pipeline = pipelines.find(p => p.name === 'simple-flow');
-            const nodesMap = new NodesMap(pipeline);
-            const n1 = pipeline.nodes[0].nodeName;
-            const n2 = pipeline.nodes[1].nodeName;
-            const n3 = pipeline.nodes[2].nodeName;
-            nodesMap.setEdge(n1, n2, { prop: 5 });
-            nodesMap.setEdge(n2, n3, { prop: 6 });
-            const edges = nodesMap.getEdges();
-            expect(edges[0].value).to.have.property('prop');
-            expect(edges[1].value).to.have.property('prop');
-        });
+describe('Edges', () => {
+    it('should create edge', () => {
+        const pipeline = pipelines.find(p => p.name === 'simple-flow');
+        const n1 = pipeline.nodes[0].nodeName;
+        const n2 = pipeline.nodes[3].nodeName;
+        const dag1 = new NodesMap(pipeline);
+        const edge1 = dag1.getEdge(n1, n2);
+        const dag2 = new NodesMap({ ...pipeline, edges: [{ source: n1, target: n2 }] });
+        const edge2 = dag2.getEdge(n1, n2);
+        expect(edge1).to.be.undefined;
+        expect(edge2).to.have.property('types');
+    });
+    it('should getEdge', () => {
+        const pipeline = pipelines.find(p => p.name === 'simple-flow');
+        const n1 = pipeline.nodes[0].nodeName;
+        const n2 = pipeline.nodes[1].nodeName;
+        const nodesMap = new NodesMap(pipeline);
+        const edge = nodesMap.getEdge(n1, n2);
+        expect(edge).to.have.property('types');
+    });
+    it('should setEdge', () => {
+        const pipeline = pipelines.find(p => p.name === 'simple-flow');
+        const n1 = pipeline.nodes[0].nodeName;
+        const n2 = pipeline.nodes[1].nodeName;
+        const nodesMap = new NodesMap(pipeline);
+        nodesMap.setEdge(n1, n2, { prop: 5 });
+        const edge = nodesMap.getEdge(n1, n2)
+        expect(edge).to.have.property('prop');
+        expect(edge).to.not.have.property('types');
+    });
+    it('should updateEdge', () => {
+        const pipeline = pipelines.find(p => p.name === 'simple-flow');
+        const n1 = pipeline.nodes[0].nodeName;
+        const n2 = pipeline.nodes[1].nodeName;
+        const nodesMap = new NodesMap(pipeline);
+        nodesMap.updateEdge(n1, n2, { prop: 5 });
+        const edge = nodesMap.getEdge(n1, n2)
+        expect(edge).to.have.property('prop');
+        expect(edge).to.have.property('types');
+    });
+    it('should get edge types', () => {
+        const pipeline = pipelines.find(p => p.name === 'simple-flow');
+        const n1 = pipeline.nodes[0].nodeName;
+        const n2 = pipeline.nodes[1].nodeName;
+        const nodesMap = new NodesMap(pipeline);
+        const types = nodesMap.getEdgeTypes(n1, n2);
+        expect(types).to.have.lengthOf(2);
+    });
+    it('should get empty edge types', () => {
+        const pipeline = pipelines.find(p => p.name === 'simple-flow');
+        const n1 = pipeline.nodes[0].nodeName;
+        const n2 = pipeline.nodes[3].nodeName;
+        const nodesMap = new NodesMap(pipeline);
+        const types = nodesMap.getEdgeTypes(n1, n2);
+        expect(types).to.have.lengthOf(0);
+    });
+    it('should getEdges', () => {
+        const pipeline = pipelines.find(p => p.name === 'simple-flow');
+        const nodesMap = new NodesMap(pipeline);
+        const n1 = pipeline.nodes[0].nodeName;
+        const n2 = pipeline.nodes[1].nodeName;
+        const n3 = pipeline.nodes[2].nodeName;
+        nodesMap.setEdge(n1, n2, { prop: 5 });
+        nodesMap.setEdge(n2, n3, { prop: 6 });
+        const edges = nodesMap.getEdges();
+        expect(edges[0].value).to.have.property('prop');
+        expect(edges[1].value).to.have.property('prop');
     });
 });
--- tests/persistency.js
+++ tests/persistency.js
@@ -1,6 +1,6 @@
 const { expect } = require('chai');
 const { uuid: uuidv4 } = require('@hkube/uid');
-const NodesMap = require('../lib/graph/nodes-map');
+const NodesMap = require('../lib/graph/dag');
 const { Persistency } = require('../index');
 const pipelines = require('./pipelines.json');
 
@@ -17,49 +17,47 @@ const dbConfig = {
     }
 };
 
-describe('DAG', () => {
-    describe('Persistency', () => {
-        it('should save and get graph', async () => {
-            const nodesMap = new NodesMap(pipelines[0]);
-            const persistency = new Persistency()
-            await persistency.init({ connection: dbConfig })
-            const jobId = `jobId-${uuidv4()}`;
-            const data = { ...nodesMap.getJSONGraph(), jobId, timestamp: Date.now() };
-            await persistency.setGraph({ jobId, data });
-            const getRes = await persistency.getGraph({ jobId });
-            expect(getRes).to.eql(data);
-        });
+describe('Persistency', () => {
+    it('should save and get graph', async () => {
+        const nodesMap = new NodesMap(pipelines[0]);
+        const persistency = new Persistency()
+        await persistency.init({ connection: dbConfig })
+        const jobId = `jobId-${uuidv4()}`;
+        const data = { ...nodesMap.getJSONGraph(), jobId, timestamp: Date.now() };
+        await persistency.setGraph({ jobId, data });
+        const getRes = await persistency.getGraph({ jobId });
+        expect(getRes).to.eql(data);
+    });
 
-        it('should return null if no graph', async () => {
-            const nodesMap = new NodesMap(pipelines[0]);
-            const persistency = new Persistency()
-            await persistency.init({ connection: dbConfig })
-            const jobId = `jobId-${uuidv4()}`;
-            const data = { ...nodesMap.getJSONGraph(), jobId, timestamp: Date.now() };
-            await persistency.setGraph({ jobId, data: null });
-            const getRes = await persistency.getGraph({ jobId });
-            expect(getRes).to.not.exist;
-        });
+    it('should return null if no graph', async () => {
+        const nodesMap = new NodesMap(pipelines[0]);
+        const persistency = new Persistency()
+        await persistency.init({ connection: dbConfig })
+        const jobId = `jobId-${uuidv4()}`;
+        const data = { ...nodesMap.getJSONGraph(), jobId, timestamp: Date.now() };
+        await persistency.setGraph({ jobId, data: null });
+        const getRes = await persistency.getGraph({ jobId });
+        expect(getRes).to.not.exist;
+    });
 
-        it('should update current graph', async () => {
-            const nodesMap = new NodesMap(pipelines[0]);
-            const persistency = new Persistency()
-            await persistency.init({ connection: dbConfig })
-            const jobId = `jobId-${uuidv4()}`;
-            const data = { ...nodesMap.getJSONGraph(), jobId, timestamp: Date.now() };
-            await persistency._adapter._db.jobs.create({ jobId, graph: data })
-            const updated = { options: {}, nodes: [], edges: [], jobId, timestamp: Date.now() };
-            await persistency.setGraph({ jobId, data: updated });
-            const getRes = await persistency.getGraph({ jobId });
-            expect(getRes).to.eql(updated);
-        });
+    it('should update current graph', async () => {
+        const nodesMap = new NodesMap(pipelines[0]);
+        const persistency = new Persistency()
+        await persistency.init({ connection: dbConfig })
+        const jobId = `jobId-${uuidv4()}`;
+        const data = { ...nodesMap.getJSONGraph(), jobId, timestamp: Date.now() };
+        await persistency._adapter._db.jobs.create({ jobId, graph: data })
+        const updated = { options: {}, nodes: [], edges: [], jobId, timestamp: Date.now() };
+        await persistency.setGraph({ jobId, data: updated });
+        const getRes = await persistency.getGraph({ jobId });
+        expect(getRes).to.eql(updated);
+    });
 
-        it('should fail to get non-exist graph', async () => {
-            const persistency = new Persistency()
-            await persistency.init({ connection: dbConfig })
-            const jobId = `jobId-${uuidv4()}`;
-            const getRes = await persistency.getGraph({ jobId });
-            expect(getRes).to.not.exist;
-        });
+    it('should fail to get non-exist graph', async () => {
+        const persistency = new Persistency()
+        await persistency.init({ connection: dbConfig })
+        const jobId = `jobId-${uuidv4()}`;
+        const getRes = await persistency.getGraph({ jobId });
+        expect(getRes).to.not.exist;
     });
 });
