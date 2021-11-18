d3809052fb0e2ef27a3bef8840754366bb401bec
NassiHarel <nassi9@gmail.com>
2021-11-18 11:17:00 +0200

    feat: pers

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const NodesMap = require('./lib/dag/dag');
 const NodeStates = require('./lib/const/NodeStates');
 const NodeTypes = require('./lib/dag/index');
-const Persistency = require('./lib/persistency/persistency');
 
 module.exports = {
     NodesMap,
     NodeStates,
-    NodeTypes,
-    Persistency
+    NodeTypes
 };
